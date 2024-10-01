49842f84b0e77e05512d4b4537b9fabbe9c556ec
NexusCoders-cyber <ilomraphael@gmail.com>
2024-10-01 21:43:44 +0100

    Create config.js
    NexusCoders config.js..

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    botName: 'NexusCoders',
+    prefix: '!',
+    rateLimitWindow: 60000,
+    rateLimitMax: 5,
+    dbPath: './bot.sqlite',
+    logPath: './bot.log',
+    timeZone: 'UTC'
+};
