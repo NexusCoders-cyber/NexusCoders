02eefec0adeca6d3642b50df4817d2fbd71c978b
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-02 22:18:28 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,23 +1,26 @@
 const winston = require('winston');
 const config = require('./config');
 
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        winston.format.errors({ stack: true }),
-        winston.format.splat(),
-        winston.format.json()
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        customFormat
     ),
-    defaultMeta: { service: 'whatsapp-bot' },
     transports: [
-        new winston.transports.File({ filename: config.logPath }),
+        new winston.transports.File({ 
+            filename: config.logPath,
+            maxsize: 5242880,
+            maxFiles: 5,
+        }),
         new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.colorize(),
-                winston.format.simple()
+                customFormat
             )
         })
     ]
