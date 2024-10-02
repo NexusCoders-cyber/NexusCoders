9a71441ae787e4f61b65afc240ffb24455205247
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-02 22:32:03 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,16 +6,12 @@ module.exports = {
   name: 'help',
   description: 'List all available commands or get specific command info',
   async execute(client, message, args) {
-    try {
-      const currentTime = new Date().toLocaleTimeString();
-      const username = message.from.split('@')[0];
-      const commandFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.js'));
+    const currentTime = new Date().toLocaleTimeString();
+    const username = message.from.split('@')[0];
+    const commandFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.js'));
 
-      if (!args.length) {
-        let helpText = '';
-
-        helpText += `
-Hello ${username} ${currentTime}
+    if (!args.length) {
+      let helpText = `Hello ${username} ${currentTime}
 ❄ Creator : NexusCoders
 ❄ User : ${username}
 ❄ Status : free
@@ -24,38 +20,33 @@ Hello ${username} ${currentTime}
 ❄ Prefix : !
 
 *COMMANDS*
-┏━━━━━━━━━━━`;
+┏━━━━━━━━━━━\n`;
 
-        for (const file of commandFiles) {
-          const command = require(path.join(__dirname, file));
-          helpText += `❏!${command.name}: ${command.description}\n`;
-        }
-
-        helpText += `
-┗━━━━━━━━━━
- 𝗰𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 𝗡𝗲𝘅𝘂𝘀𝗖𝗼𝗱𝗲𝗿𝘀`;
-
-        await message.reply(helpText);
-      } else {
-        const commandName = args[0].toLowerCase();
-        const command = client.commands.get(commandName);
-
-        if (!command) {
-          return message.reply(`Command "${commandName}" not found.`);
-        }
-
-        const helpText = `
-Command: !${command.name}\n
-Description: ${command.description}\n
-`;
-
-        await message.reply(helpText);
+      for (const file of commandFiles) {
+        const command = require(path.join(__dirname, file));
+        helpText += `❏!${command.name}: ${command.description}\n`;
       }
 
-      logger.info(`Help command executed by ${username}`);
-    } catch (error) {
-      logger.error(`Error in help command: ${error}`);
-      throw error;
+      helpText += `┗━━━━━━━━━━
+𝗰𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 𝗡𝗲𝘅𝘂𝘀𝗖𝗼𝗱𝗲𝗿𝘀`;
+
+      await client.sendMessage(message.from, helpText);
+    } else {
+      const commandName = args[0].toLowerCase();
+      const command = commandFiles.find(file => file.toLowerCase() === `${commandName}.js`);
+
+      if (!command) {
+        await client.sendMessage(message.from, `Command "${commandName}" not found.`);
+        return;
+      }
+
+      const commandModule = require(path.join(__dirname, command));
+      const helpText = `Command: !${commandModule.name}
+Description: ${commandModule.description}`;
+
+      await client.sendMessage(message.from, helpText);
     }
+
+    logger.info(`Help command executed by ${username}`);
   },
 };
