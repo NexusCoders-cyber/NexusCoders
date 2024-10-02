9742d1c19e6078d1e047d2ce5e475076aca99dac
Asta ichiyukimori <162071948+Asta-Bot-V2@users.noreply.github.com>
2024-10-02 08:40:55 +0100

    Update commands

---
+++ commands/translate.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const logger = require('../logger');
+
+module.exports = {
+  name: 'translate',
+  description: 'Translate text to desired language',
+  async execute(client, message, args) {
+    try {
+      const langCodeTrans = args[1] ? args[1] : 'en';
+      const content = args[0] ? args[0] : message.quotedMsg ? message.quotedMsg.body : '';
+
+      if (!content) return message.reply('Please provide text to translate');
+
+      const { text, lang } = await translate(content.trim(), langCodeTrans.trim());
+      const translationInfo = `Translated from ${lang} to ${langCodeTrans}\n\n`;
+
+      message.reply(translationInfo + text);
+      logger.info(`Translation command used by ${message.from}`);
+    } catch (error) {
+      logger.error(`Error in translate command: ${error}`);
+      message.reply('Error translating text. Please try again.');
+    }
+  },
+};
+
+async function translate(text, langCode) {
+  const res = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${langCode}&dt=t&q=${encodeURIComponent(text)}`);
+  return { text: res.data[0].map(item => item[0]).join(''), lang: res.data[2] };
+}
