fe35013ba704962b6c6591ce421bbc7a7293d703
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-03 08:50:02 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,28 +1,15 @@
 const winston = require('winston');
-const config = require('./config');
-
-const customFormat = winston.format.printf(({ level, message, timestamp }) => {
-    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-});
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        customFormat
+        winston.format.timestamp(),
+        winston.format.json()
     ),
     transports: [
-        new winston.transports.File({ 
-            filename: config.logPath,
-            maxsize: 5242880,
-            maxFiles: 5,
-        }),
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                customFormat
-            )
-        })
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
     ]
 });
 
