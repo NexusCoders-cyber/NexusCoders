fd1636db1297c37933fe83b956e1c7ea09bf97c0
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-03 08:49:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
     prefix: '!',
-    rateLimitWindow: 60000,
-    rateLimitMax: 5,
     timeZone: 'UTC',
-    dbPath: './nexuscoders_bot.sqlite',
-    logPath: './nexuscoders_bot.log',
-    botName: 'NexusCoders Bot',
-    version: '1.0.0',
-    owner: 'NexusCoders Team'
+    rateLimitWindow: 60000,
+    rateLimitMax: 5
 };
